67c698de946416234219b9d5f4078aa4b3bc6a60
Yunhao Yang <yunhaoyang@Yunhaos-Mini.lan>
2023-11-04 17:08:42 -0500

    add main

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 from LLM_Interaction import *
 from ModelChecking import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--task_name', type=str, default='CrossRoad')
+parser.add_argument('--task_description', type=str, default='go straight at an intersection without traffic light')
+parser.add_argument('--model_path', type=str, default='examples/sample_model.smv')
+parser.add_argument('--spec_path', type=str, default='examples/sample_ltl.txt')
+parser.add_argument('--api_path', type=str, default='examples/sample_api.py')
+
+def main(args):
+	task = args.task_description
+	model_path, spec_path, api_path = args.model_path, args.spec_path, args.api_path
+	steps = gen_steps(model_path, task, 3)
+	nusmv = text2automaton(steps, model_path, False)
+	text2code(steps, api_path, args.task_name)
+
+	automaton_path = 'NuSMV/temp/task.smv'
+	model_checking(automaton_path, spec_path)
+	show_stats()
+
+if __name__ == '__main__':
+    main(parser.parse_args())
--- output/CrossRoad.py
+++ output/CrossRoad.py
@@ -2,7 +2,10 @@ def CrossRoad(vehicle):
     vehicle.observe()  # observe the environment first
     if vehicle.pedestrian_observed():  # Step 1
         vehicle.stop()  # stop if pedestrian is observed
+        return "Stop"
     elif vehicle.car_observed():  # Step 2
         vehicle.stop()  # stop if car is observed
+        return "Stop"
     else:  # Step 3
         vehicle.move_forward()  # move forward if neither pedestrian nor car is observed
+        return "Move_forward"
